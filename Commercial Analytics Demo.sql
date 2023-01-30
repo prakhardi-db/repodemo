8aa86f6ffa0967f564548c691a4b401e1b4ca17f
prakhardi-db <91159579+prakhardi-db@users.noreply.github.com>
2023-01-30 09:27:29 -0800

    Add files via upload

---
+++ Commercial Analytics Demo.sql
@@ -0,0 +1,95 @@
+-- Databricks notebook source
+SELECT
+  COUNT(distinct(s_suppkey)) AS num_suppliers
+FROM
+  samples.tpch.supplier
+
+-- COMMAND ----------
+
+SELECT
+  o_orderdate AS Date,
+  o_orderpriority AS Priority,
+  sum(o_totalprice) AS `Total Price`
+FROM
+  `samples`.`tpch`.`orders`
+WHERE
+  o_orderdate > '1994-01-01'
+  AND o_orderdate < '1994-01-31'
+GROUP BY
+  1,
+  2
+ORDER BY
+  1,
+  2
+
+-- COMMAND ----------
+
+SELECT
+    year(o_orderdate) AS year,
+    n_name AS nation,
+    sum(l_extendedprice * (1 - l_discount) * (((length(n_name))/100) + (year(o_orderdate)-1993)/100)) AS revenue
+FROM
+    `samples`.`tpch`.`customer`,
+    `samples`.`tpch`.`orders`,
+    `samples`.`tpch`.`lineitem`,
+    `samples`.`tpch`.`supplier`,
+    `samples`.`tpch`.`nation`,
+    `samples`.`tpch`.`region`
+WHERE
+    c_custkey = o_custkey
+    AND l_orderkey = o_orderkey
+    AND l_suppkey = s_suppkey
+    AND c_nationkey = s_nationkey
+    AND s_nationkey = n_nationkey
+    AND n_regionkey = r_regionkey
+    AND n_name in ('ARGENTINA', 'UNITED KINGDOM', 'FRANCE','BRAZIL', 'CHINA', 'UNITED STATES', 'JAPAN', 'JORDAN')
+    AND o_orderdate >= DATE '1994-01-01'
+GROUP BY
+    1,2
+ORDER BY
+    nation ASC LIMIT 1000;
+
+-- COMMAND ----------
+
+SELECT
+    initcap(n_name) AS Nation, 
+    SUM(l_extendedprice * (1 - l_discount) * (length(n_name)/100)) AS revenue
+FROM
+    samples.tpch.customer,
+    samples.tpch.orders,
+    samples.tpch.lineitem,
+    samples.tpch.supplier,
+    samples.tpch.nation,
+    samples.tpch.region
+WHERE
+    c_custkey = o_custkey
+    AND l_orderkey = o_orderkey
+    AND l_suppkey = s_suppkey
+    AND c_nationkey = s_nationkey
+    AND s_nationkey = n_nationkey
+    AND n_regionkey = r_regionkey
+GROUP BY
+    INITCAP(n_name)
+ORDER BY
+    revenue DESC;
+
+-- COMMAND ----------
+
+-- profile the data
+Select * from samples.tpch.customer
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC pip install bamboolib
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC import bamboolib as bam
+-- MAGIC bam
+
+-- COMMAND ----------
+
+
+
